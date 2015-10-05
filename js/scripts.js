6395886b62187035c2aac537437b1cd22effe35c
Ariel Spear <arielspear@github.com>
2015-10-05 13:49:32 -0700

    switching projects for the day

--- js/scripts.js
+++ js/scripts.js
@@ -26,8 +26,43 @@ var makePrice = function(thePizza) {
   } else if (thePizza.pizzaSize === "large") {
     var base = 12;
   } else var base = 0;
-  
+
   var toppingPrice = thePizza.toppings.length;
   var result = "$" + (base + toppingPrice);
   return result;
 }
+
+
+$(document).ready(function() {
+  $(document).on("click", "input", function() {
+    $("#total").text(makePrice());
+    $("#price").show();
+  });
+
+  $(document).on("click", ".remove", function() {
+      $(this).parent().remove();
+      $("#total").text(totalPrice());
+  });
+
+  var quantity = 1;
+  $("#add").click(function() {
+    quantity++;
+    $("#order").append('<div class="pizza col-md-4">' +
+                        '<div id="size'+quantity+'">' +
+                          '<h4>Select a size:</h4>' +
+                          '<input type="radio" name="size'+quantity+'" value="small">small' +
+                          '<input type="radio" name="size'+quantity+'" value="medium">medium' +
+                          '<input type="radio" name="size'+quantity+'" value="large">large' +
+                        '</div>' +
+                        '<div id="toppings'+quantity+'">' +
+                          '<h4>Select toppings:</h4>' +
+                          '<input type="checkbox" name="topping'+quantity+'" value="pepperoni"> Pepperoni<br>' +
+                          '<input type="checkbox" name="topping'+quantity+'" value="sausage"> Sausage<br>' +
+                          '<input type="checkbox" name="topping'+quantity+'" value="ham"> Ham<br>' +
+                          '<input type="checkbox" name="topping'+quantity+'" value="pineapple"> Pineapple<br>' +
+                          '<input type="checkbox" name="topping'+quantity+'" value="jalapenos"> Jalapenos<br>' +
+                          '<input type="checkbox" name="topping'+quantity+'" value="olives"> Olives<br>' +
+                        '</div>' +
+                      '</div>');
+  });
+});
