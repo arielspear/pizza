e75837e4efadfe97197629d1301d6a4f093ca677
Ariel Spear <simpleredfox@gmail.com>
2015-10-04 23:21:58 -0700

    test object for pizza-ness, and test getToppings method

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-describe('firstTest', function(){
-  it("has a test", function(){
-    expect(firstFunction()).to.equal(true);
-  })
-});
+var pizza = function(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+var makePizza = function (size, toppings) {
+  var result = new pizza(size, toppings);
+  return result;
+}
+
+var getToppings = function (thePizza) {
+  var result = thePizza.toppings;
+  return result;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,12 @@
-var firstFunction = function() {
-  return false;
-}
+describe('makePizza', function(){
+  it("returns a pizza object", function(){
+    expect(makePizza("large", ["cheese"])).to.be.an.instanceof(pizza);
+  })
+});
+
+describe('getToppings', function(){
+  var thePizza = makePizza("large", ["cheese", "sausage"]);
+  it("returns correct toppings", function(){
+    expect(getToppings(thePizza)).to.eql(["cheese", "sausage"]);
+  })
+});
